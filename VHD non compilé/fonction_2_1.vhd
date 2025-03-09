66892631e6f415d62ae5070bf4708f51018b5a44
jujububule <julien.talent01@gmail.com>
2025-03-09 11:36:27 +0100

    code commenté

--- VHD non compilé/fonction_2_1.vhd
+++ VHD non compilé/fonction_2_1.vhd
@@ -1,8 +1,10 @@
+-- Importations des librairies
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 use ieee.std_logic_arith.all;
 
+-- Definition des entrées
 entity fonction_2_1 is
 port(
     clk_in : in std_logic;
@@ -12,23 +14,24 @@ port(
 );
 end fonction_2_1;
 
+-- Le programme
 architecture chenilleDGDG of fonction_2_1 is
-
 begin
-    process(clk_in, key2)
+
+    process(clk_in, key2)-- En cas de changement de l'une de ces entrées nous allons procédé a l'actualisation du code suivant
     begin
-        if key2 = '0' then
+        if key2 = '0' then-- Remise à 0
             hex3 <= "0000000";
 
-        elsif rising_edge(clk_in)then
-            case hex3 is --permet de décalé la led allumé
+        elsif rising_edge(clk_in)then-- On changera l'affichage uniquement si l'on est sur un front montant de l'horloge
+            case hex3 is-- Permet de décalé la led allumé
                 when "1111110" => hex3 <= "1111101";
                 when "1111101" => hex3 <= "1111011";
                 when "1111011" => hex3 <= "1110111";
                 when "1110111" => hex3 <= "1101111";
                 when "1101111" => hex3 <= "1011111";
                 when "1011111" => hex3 <= "0111111";
-                when others => hex3 <= "1111110";--permet l'initialisation
+                when others => hex3 <= "1111110";-- Permet l'initialisation
             end case;
         end if;
     end process;
