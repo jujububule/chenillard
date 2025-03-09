db1f1f46113e1ce58b9f0b327e0231636a1e60c3
jujububule <julien.talent01@gmail.com>
2025-03-09 10:56:41 +0100

    Update fonction_2_4.vhd

--- VHD non compilé/fonction_2_4.vhd
+++ VHD non compilé/fonction_2_4.vhd
@@ -1,8 +1,10 @@
+-- Importations des librairies
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 use ieee.std_logic_arith.all;
 
+-- Definition des entrées
 entity fonction_2_4 is
 port(
     clk_in : in std_logic;
@@ -15,29 +17,27 @@ port(
 );
 end fonction_2_4;
 
+-- Le programme
 architecture chenille of fonction_2_4 is
-
 begin
-
-
 
     process(clk_in, key2)
     begin
-        if key2 = '0' then
+        if key2 = '0' then-- Remise a 0
             hex0 <= "0000000";
             hex1 <= "0000000";
             hex2 <= "0000000";
             hex3 <= "0000000";
 
         elsif rising_edge(clk_in)then
-            case hex0 is -- affiché 5
-                when "0010111" =>
+            case hex0 is
+                when "0010111" =>-- Etein les segments
                     hex0 <= "1111111";
                     hex1 <= "1111111";
                     hex2 <= "1111111";
                     hex3 <= "1111111";
-                when others =>
-                    hex0 <= "0010111";--permet l'initialisation
+                when others =>-- Permet l'initialisation et l'affichage de 2025
+                    hex0 <= "0010111";
                     hex1 <= "0100100";
                     hex2 <= "1000000";
                     hex3 <= "0100100";
